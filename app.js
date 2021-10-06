f39d38d4bc75e20106976b3c92790c94a14bcdb0
TrevorRezac <trev.rezac@gmail.com>
2021-10-06 15:48:32 -0700

    added image click counter

--- app.js
+++ app.js
@@ -8,18 +8,31 @@ const dogSound = document.getElementById('dog-sound');
 const horseImage = document.getElementById('horse-image');
 const horseSound = document.getElementById('horse-sound');
 
+//"state" - things that change over time
+let catClicks = 0;
+let dogClicks = 0;
+let horseClicks = 0;
+
 // set event listeners 
     // get user input(s)
     // do any needed work with the value(s)
     // update DOM to reflect new value(s)
 catImage.addEventListener('click', () => {
     catSound.play();
+    catClicks = catClicks + 1;
+    console.log('cat clicks is now', catClicks);
 });
+
 
 dogImage.addEventListener('click', () => {
     dogSound.play();
+    dogClicks = dogClicks + 1;
+    console.log('dog clicks is now', dogClicks)
 });
 
 horseImage.addEventListener('click', () => {
     horseSound.play();
+    horseClicks = horseClicks + 1;
+    console.log('horse clicks is now', horseClicks);
 });
+
